99497c78f74c15242341e94e9f6db3323185e4fb
RatajewskiMaciej <ratajewskimaciej@wp.pl>
2024-02-01 12:28:30 +0100

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header.jsx"
+import Footer from './components/Footer.jsx'
+import HomeScreen from './screens/HomeScreen.jsx'
 
 const App = () => {
   return (
-    <div>Welcome to ProShop</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
